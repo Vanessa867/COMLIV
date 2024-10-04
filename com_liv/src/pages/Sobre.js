8c0c82dc06c9e88a59b9d0c31f2d1dcdc19c6882
Vanessa <vanessalins356@gmail.com>
2024-10-04 15:48:42 -0300

    AltaraçõesPAG-SOBRE

--- com_liv/src/pages/Sobre.js
+++ com_liv/src/pages/Sobre.js
@@ -1,8 +1,14 @@
+import '../Styles/Sobre.css';
+import React from 'react'
+import images from "./images/home (1).png";
+
 const Sobre = () => {
     return (
-      <div>
+      <div className='container'>
         <h1>Sobre nós</h1>
-        <p>Welcome to the Livros page!</p>
+        <p>Somos uma equipe <strong>apaixonada por leitura</strong>, comprometida em facilitar o acesso a clubes de leitura e promover o <strong>incentivo à leitura</strong>em todo o Brasil</p>
+        <img className='imagem' src={images} alt='imagem da home do site'/>
+        <p>Essa é a <strong>essência do nosso clube:</strong> prezamos pela simplicidade, garantindo que o entendimento seja acessível para <strong>diferentes gerações.</strong></p>
       </div>
     );
   };
