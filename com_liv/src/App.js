17572bdd8a0094fd437af926525838f6505d9436
Vanessa <vanessalins356@gmail.com>
2024-10-04 16:16:17 -0300

    Atualização do appjshome

--- com_liv/src/App.js
+++ com_liv/src/App.js
@@ -1,19 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Sobre from './pages/Sobre';
-import Planos from './pages/Planos';
 
 const App = () => {
   return (
-    <Router>
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Sobre" element={<Sobre />} />
-        <Route path="/Planos" element={<Planos />} />
-      </Routes>
+      <Home />
     </div>
-  </Router>
   );
 };
 
